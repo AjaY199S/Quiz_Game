53a68e7e12e750dd526d5cf534fe818d482571a1
Ajay Pratap Singh Thakur <thakurajaypratapsingh877@gmial.com>
2019-10-13 17:33:30 +0530

    first commit

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour {
+
+	public GameObject button_name;
+	public GameObject button_name1;
+	public bool muteI;
+
+	// Use this for initialization
+	void Start () {
+		button_name.SetActive (true);
+		button_name1.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void ShowPanel()
+	{
+		button_name.gameObject.SetActive (false);
+		button_name1.gameObject.SetActive (true);
+	}
+	public void Quit_Game()
+	{
+		Debug.Log ("Game Is Out");
+		Application.Quit();
+	}
+	public void MUTEfuc()
+	{
+		muteI =! muteI;
+		AudioListener.volume = muteI ? 0 : 1;
+	}
+}
